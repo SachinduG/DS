295e290c9dc02b5e6fba7cd7d29c2bff3e24ffb4
Sachindu <sachindugimhana0@gmail.com>
2021-05-13 22:06:38 +0530

    second coomit

--- Seller/shopping-store-backend/src/main/java/net/shoppingstore/supplyitemsapi/exception/UserAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package net.shoppingstore.supplyitemsapi.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-/* Custom Exception already created and to be used */
-@ResponseStatus(code = HttpStatus.CONFLICT, reason = "User already exists")
-public class UserAlreadyExistsException extends Exception {
-}
--- Seller/shopping-store-backend/src/main/java/net/shoppingstore/supplyitemsapi/exception/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package net.shoppingstore.supplyitemsapi.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-/* Custom Exception already created and to be used */
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Specified User Not Found")
-public class UserNotFoundException extends Exception {
-}
